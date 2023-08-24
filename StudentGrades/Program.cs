3fb0011f5c1ecde8939d15614f59039437d069a7
usmanali-3 <39674566+usmanali-3@users.noreply.github.com>
2023-08-24 15:43:53 +0500

    Add files via upload

---
+++ StudentGrades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StudentGrades
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            // Get Percentage 
+            var gradeCalculator = new GradeCalculator();
+
+            Console.Write("Enter The Percentage : ");
+
+            var percentage = Convert.ToInt32(Console.ReadLine());
+
+            var grade = gradeCalculator.GetGradeByPercentage(percentage);
+
+            // print Grade
+            Console.WriteLine($"Student Grade : {grade}");
+        }
+    }
+}
